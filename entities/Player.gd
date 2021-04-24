690c73c2af89a1c8bd5001364bb74b9aa6715f7c
Carston Schilds <carston.schilds@protonmail.com>
2021-04-24 10:46:01 -0700

    working on sonar

--- entities/Player.gd
+++ entities/Player.gd
@@ -1,12 +1,24 @@
 extends "res://entities/Entity.gd"
 
 onready var sprite = $SpritePosition/AnimatedSprite
+onready var sonar = $SonarParticle
+var sonar_range = 300
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(global_position, self.facing.rotated(-PI/2).normalized() * sonar_range, [self])
+	if 'position' in result:
+		draw_sonar_hit(result)
+	
+func draw_sonar_hit(ray_cast_result):
+	var particle = sonar.duplicate()
+	particle.global_position = ray_cast_result.position	
 	
 func get_inputs():
 	var input_vector = Vector2.ZERO
