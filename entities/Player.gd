35fe23652ad556bdf505720f4da051911073bb97
Carston Schilds <carston.schilds@protonmail.com>
2021-04-24 11:10:18 -0700

    sonar working

--- entities/Player.gd
+++ entities/Player.gd
@@ -3,6 +3,8 @@ extends "res://entities/Entity.gd"
 onready var sprite = $SpritePosition/AnimatedSprite
 onready var sonar = $SonarParticle
 var sonar_range = 300
+var sonar_delta = 0
+var sonar_active = false
 
 func _ready():
 	pass # Replace with function body.
@@ -11,23 +13,34 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	var space_state = get_world_2d().direct_space_state
-	var result = space_state.intersect_ray(global_position, self.facing.rotated(-PI/2).normalized() * sonar_range, [self])
-	if 'position' in result:
-		draw_sonar_hit(result)
+	if sonar_active:
+		var space_state = get_world_2d().direct_space_state
+		for i in range(-8, 8):
+			var angle_delta = PI/64 * i
+			var result = space_state.intersect_ray(global_position, self.facing.rotated(-PI/2 + angle_delta).normalized() * sonar_range, [self])
+			if 'position' in result:
+				draw_sonar_hit(result)
+		sonar_active = false
+		print('sonar off!')
 	
 func draw_sonar_hit(ray_cast_result):
 	var particle = sonar.duplicate()
-	particle.global_position = ray_cast_result.position	
+	self.get_parent().add_child(particle)
+	particle.global_position = ray_cast_result.position
+	particle.emitting = true
 	
 func get_inputs():
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("rotate_right") - Input.get_action_strength("rotate_left")
 	input_vector.y = Input.get_action_strength("forward_thrust") - Input.get_action_strength("reverse_thrust")
-
+	
 	if input_vector.y != 0:
 		sprite.animation = "move"
 	else:
 		sprite.animation = "idle"
+		
+	if Input.is_action_just_released("sonar_toggle"):
+		print('sonar active!')
+		self.sonar_active = true
 
 	return input_vector
