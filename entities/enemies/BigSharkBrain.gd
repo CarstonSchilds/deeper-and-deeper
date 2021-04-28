f70385b0960034de9f1c56529c52b1a30dfb23db
Colin Heathman <Colin Heathman>
2021-04-27 20:19:03 -0700

    Made enemies a little more likely to leave you alone, fixed UI alignment, added Zoom to Controls label, made glow a little brighter

--- entities/enemies/BigSharkBrain.gd
+++ entities/enemies/BigSharkBrain.gd
@@ -3,5 +3,5 @@ extends "res://entities/enemies/Brain.gd"
 func _init():
 	self.sonar_sensitive = true
 	self.normal_patrol_time = 3.0
-	self.leash_range = 800
-	$"States/Rest/Timer".wait_time = 3
+	self.leash_range = 400
+	$"States/Rest/Timer".wait_time = 4
--- entities/enemies/SquidBrain.gd
+++ entities/enemies/SquidBrain.gd
@@ -4,5 +4,5 @@ func _init():
 	self.light_sensitive = true
 	self.no_patrol = true
 	self.normal_patrol_time = 5.0
-	self.leash_range = 1000
+	self.leash_range = 500
 	$"States/Rest/Timer".wait_time = 3
